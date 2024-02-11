ce057c211df6e1982c2a3700eb78f146911adf53
mirek0707 <mirek.k2000@interia.eu>
2024-02-11 18:42:54 +0100

    add scraping by year
    Co-authored-by: pSzybko <sipkopawel@student.agh.edu.pl>

--- lubimyczytac/lubimyczytac/spiders/books.py
+++ lubimyczytac/lubimyczytac/spiders/books.py
@@ -5,25 +5,34 @@ import re
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["lubimyczytac.pl"]
-    start_urls = ["http://lubimyczytac.pl/katalog"]
+    start_urls = ["https://lubimyczytac.pl/katalog?listId=booksFilteredList&rating[0]=0&rating[1]=10&publishedYear[0]=2024&publishedYear[1]=2024&catalogSortBy=ratings-desc&paginatorType=Standard"]
     curr_page_num = 1
+    curr_year = 2024
 
     def parse(self, response):
         books = response.xpath("//div[@class='authorAllBooks__single']/div/div[@class='col authorAllBooks__singleCenter authorAllBooks__singleCenter--list']/div/div/a[@class='authorAllBooks__singleTextTitle float-left']")
         for book in books:
-            # title = book.xpath("normalize-space(.//text())").get()
             url = book.xpath(".//@href").get()
 
             yield response.follow(url=url, callback = self.parse_book)
-            # yield {
-            #     'title' : title,
-            #     'url': url
-            # }
+        
         self.curr_page_num += 1
         max_page_num = response.xpath("//div/div[@id='booksFilteredListPaginatorButton']/div/nav/ul/li[@class='paginationList__info']/span/text()").get()
         if (self.curr_page_num <= int(max_page_num)):
-            next_page = self.start_urls[0] + "?page=" + str(self.curr_page_num)
+            next_page = self.start_urls[0] + "&page=" + str(self.curr_page_num)
             yield scrapy.Request(url=next_page, callback=self.parse)
+        else:
+            self.curr_page_num = 1
+            if self.curr_year > 1963:
+                self.start_urls[0] = self.start_urls[0].replace(str(self.curr_year), str(self.curr_year - 1))
+                self.curr_year -=1
+                yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
+            else:
+                self.start_urls[0] = self.start_urls[0].replace(str(self.curr_year), "1200", 1)
+                self.start_urls[0] = self.start_urls[0].replace(str(self.curr_year), str(self.curr_year - 1), 1)
+                self.curr_year -=1
+                yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
+
 
     def parse_book(self, response):
         title = response.xpath("normalize-space(.//div/div[@class='title-container relative']/h1/text())").get()
