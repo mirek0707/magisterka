27a47898503ae962a7863c475faf5eda1fd4c250
pSzybko <sipkopawel@student.agh.edu.pl>
2024-02-13 16:39:14 +0100

    Add versions and fix unusual line terminators

--- flake.nix
+++ flake.nix
@@ -18,9 +18,9 @@
       pkgs.mkShell
         {
           buildInputs = [
-            pkgs.python311Full
-            pkgs.python311Packages.scrapy
-            pkgs.python311Packages.scrapy-splash
+            pkgs.python311Full:3.11.7
+            pkgs.python311Packages.scrapy:2.11.0
+            pkgs.python311Packages.scrapy-splash:0.9.0
           ];
 
           shellHook = ''
