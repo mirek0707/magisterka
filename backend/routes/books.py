9d6fa7bf5bc925b471c439694a86e10d0cb6d07d
pSzybko <sipkopawel@student.agh.edu.pl>
2024-03-08 21:27:06 +0100

    Fix full-text search and add regular search

--- backend/routes/books.py
+++ backend/routes/books.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database.db import books_collection
 from models.book import BookModel
 
@@ -14,3 +14,53 @@ async def get_one_book(isbn: str):
     book = await books_collection.find_one({'isbn': isbn})
     print(type(book))
     return book
+
+
+@router.post('/ftsearch', response_description='Full-text search in description, title, original_title, author', response_model=list[BookModel])
+async def full_text_search_book(query: str, num_of_books: int = 30):
+    if not query:
+        raise HTTPException(
+            status_code=400, detail='Query parameter cannot be empty')
+
+    if len(query) < 3:
+        raise HTTPException(
+            status_code=400, detail='Query parameter must have at least 3 characters')
+
+    projection = {"score": {"$meta": "textScore"}}
+    query = f"\"{query}\""
+    books = await books_collection.find(
+        {"$text": {"$search": query}},
+        projection=projection
+    ).to_list(length=None)
+
+    books = sorted(books, key=lambda x: x['score'], reverse=True)
+    books = books[:num_of_books]
+
+    return books
+
+
+@router.post('/search', response_description='Search by author, title or original_title', response_model=list[BookModel])
+async def search_book(query: str, num_of_books: int = 5):
+    if not query:
+        raise HTTPException(
+            status_code=400, detail='Query parameter cannot be empty')
+
+    books = await books_collection.find({
+        "$or":
+        [
+            {
+                "author": {"$regex": query, "$options": "i"}
+            },
+            {
+                "original_title": {"$regex": query, "$options": "i"}
+            },
+            {
+                "title": {"$regex": query, "$options": "i"}
+            }
+        ]
+    }).to_list(length=None)
+
+    books = sorted(books, key=lambda x: x['ratings_lc_number'], reverse=True)
+    books = books[:num_of_books]
+
+    return books
