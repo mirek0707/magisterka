78b30ca4c8dbb922c6b1547288898f34ef124052
pSzybko <sipkopawel@student.agh.edu.pl>
2024-04-28 15:12:59 +0200

    Refactor carousel to separate single books card from carousel item

--- frontend/src/components/carousel/item.tsx
+++ frontend/src/components/carousel/item.tsx
@@ -1,14 +1,7 @@
-import {
-  Card,
-  Grid,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-} from '@mui/material'
+import { Grid } from '@mui/material'
 import * as React from 'react'
-import { Routes } from 'src/routes'
 
+import BookCard from './card'
 import { CarouselItemProps, BooksCarouselProps } from './types'
 
 const BooksCarouselItem: React.FC<BooksCarouselProps> = ({ items }) => {
@@ -16,56 +9,7 @@ const BooksCarouselItem: React.FC<BooksCarouselProps> = ({ items }) => {
     <Grid container direction="row" spacing={{ lg: 1, xl: 2 }}>
       {items.map((item: CarouselItemProps, index: number) => (
         <Grid item xs={12 / 8} key={index} className="p-1">
-          <Card
-            className="h-full text-left relative"
-            sx={{ ':hover': { boxShadow: 10 } }}
-          >
-            <CardActionArea className="h-full" href={Routes.BookUrl(item.isbn)}>
-              <CardMedia
-                component="img"
-                className="h-3/4"
-                sx={{ objectFit: 'cover' }}
-                image={item.img_src}
-                alt="book cover"
-              />
-              <CardContent sx={{ p: 0 }} className="h-1/4">
-                <Grid
-                  className="h-full p-2"
-                  container
-                  direction="column"
-                  justifyContent="center"
-                >
-                  <Grid item>
-                    <Typography
-                      sx={{
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        display: '-webkit-box',
-                        WebkitLineClamp: '1',
-                        WebkitBoxOrient: 'vertical',
-                      }}
-                      variant="subtitle1"
-                    >
-                      {item.title}
-                    </Typography>
-                    <Typography
-                      sx={{
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        display: '-webkit-box',
-                        WebkitLineClamp: '1',
-                        WebkitBoxOrient: 'vertical',
-                        fontStyle: 'italic',
-                      }}
-                      variant="subtitle2"
-                    >
-                      {item.author}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+          <BookCard {...item} />
         </Grid>
       ))}
     </Grid>
